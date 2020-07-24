0515180226c39f460361225f29de7cf61c8d9aed
kosaka123 <messi_ong@hotmail.com>
2020-07-24 16:39:04 +0800

    first commit

---
+++ scripts.js
@@ -0,0 +1,160 @@
+//multi 2
+var a=2;
+var answerLeft=""
+
+for(var i=1;i<=3;i++)
+{
+    answerLeft+=a+"X"+i+"="+(a*i)+"<br/>";
+}
+
+document.getElementById("left-1").innerHTML=answerLeft;
+
+var a=2;
+var answerRight=""
+
+for(var i=4;i<=9;i++)
+{
+    answerRight+=a+"X"+i+"="+(a*i)+"<br/>";
+}
+
+document.getElementById("right-1").innerHTML=answerRight;
+
+//multi 3
+var b=3;
+var answerLeft=""
+
+for(var i=1;i<=3;i++)
+{
+    answerLeft+=b+"X"+i+"="+(b*i)+"<br/>";
+}
+
+document.getElementById("left-2").innerHTML=answerLeft;
+
+var answerRight=""
+
+for(var i=4;i<=9;i++)
+{
+    answerRight+=b+"X"+i+"="+(b*i)+"<br/>";
+}
+
+document.getElementById("right-2").innerHTML=answerRight;
+
+//multi 4
+var c=4;
+var answerLeft=""
+
+for(var i=1;i<=3;i++)
+{
+    answerLeft+=c+"X"+i+"="+(c*i)+"<br/>";
+}
+
+document.getElementById("left-3").innerHTML=answerLeft;
+
+var answerRight=""
+
+for(var i=4;i<=9;i++)
+{
+    answerRight+=c+"X"+i+"="+(c*i)+"<br/>";
+}
+
+document.getElementById("right-3").innerHTML=answerRight;
+
+//multi 5
+var d=5;
+var answerLeft=""
+
+for(var i=1;i<=3;i++)
+{
+    answerLeft+=d+"X"+i+"="+(d*i)+"<br/>";
+}
+
+document.getElementById("left-4").innerHTML=answerLeft;
+
+var answerRight=""
+
+for(var i=4;i<=9;i++)
+{
+    answerRight+=d+"X"+i+"="+(d*i)+"<br/>";
+}
+
+document.getElementById("right-4").innerHTML=answerRight;
+
+//multi 6
+var e=6;
+var answerLeft=""
+
+for(var i=1;i<=3;i++)
+{
+    answerLeft+=e+"X"+i+"="+(e*i)+"<br/>";
+}
+
+document.getElementById("left-5").innerHTML=answerLeft;
+
+var answerRight=""
+
+for(var i=4;i<=9;i++)
+{
+    answerRight+=e+"X"+i+"="+(e*i)+"<br/>";
+}
+
+document.getElementById("right-5").innerHTML=answerRight;
+
+//multi 7
+var f=7;
+var answerLeft=""
+
+for(var i=1;i<=3;i++)
+{
+    answerLeft+=f+"X"+i+"="+(f*i)+"<br/>";
+}
+
+document.getElementById("left-6").innerHTML=answerLeft;
+
+var answerRight=""
+
+for(var i=4;i<=9;i++)
+{
+    answerRight+=f+"X"+i+"="+(f*i)+"<br/>";
+}
+
+document.getElementById("right-6").innerHTML=answerRight;
+
+//multi 8
+var g=8;
+var answerLeft=""
+
+for(var i=1;i<=3;i++)
+{
+    answerLeft+=g+"X"+i+"="+(g*i)+"<br/>";
+}
+
+document.getElementById("left-7").innerHTML=answerLeft;
+
+var answerRight=""
+
+for(var i=4;i<=9;i++)
+{
+    answerRight+=g+"X"+i+"="+(g*i)+"<br/>";
+}
+
+document.getElementById("right-7").innerHTML=answerRight;
+
+//multi 8
+var h=9;
+var answerLeft=""
+
+for(var i=1;i<=3;i++)
+{
+    answerLeft+=h+"X"+i+"="+(h*i)+"<br/>";
+}
+
+document.getElementById("left-8").innerHTML=answerLeft;
+
+var answerRight=""
+
+for(var i=4;i<=9;i++)
+{
+    answerRight+=h+"X"+i+"="+(h*i)+"<br/>";
+}
+
+document.getElementById("right-8").innerHTML=answerRight;
